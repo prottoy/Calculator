5471303ed35171991c5850f0a7f27c090f4ed50b
prottoy <prottoy2007@gmail.com>
2016-04-11 17:19:25 +0600

    Initial Commit

---
+++ Calculator/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Mahbub Morshed on 4/11/16.
+//  Copyright © 2016 Mahbub Morshed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
